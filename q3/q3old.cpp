177ff0982585a9a50e49d12adc79af7ab706a0f1
chiefoggy <81455726+chiefoggy@users.noreply.github.com>
2022-12-22 17:46:04 +0800

    Create q3old.cpp
    does not work

---
+++ q3/q3old.cpp
@@ -0,0 +1,31 @@
+/*
+Remove duplicates
+Given an array of integers arr, create a function that returns an array containing the values of arr without duplicates (the order doesn't matter).
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int sort(int arr[])(
+    for (int i = 0; i < arr.size(); i++){
+        for (int j = (i + 1); j < arr.size(); j++){
+            if (arr[i] == arr[j]){
+                
+            }
+        }
+    }
+    
+)
+
+int main(){
+    int arr[]; cin >> arr;
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < arr.size(); i++){
+        for (int j = (i + 1); j < arr.size(); j++){
+            if (arr[i] == arr[j])
+        }
+        cout << arr[i] << " ";
+    }
+
+}
